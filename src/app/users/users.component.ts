1484f1ac4a8ceeba7c394ae4e387da7a066beaf7
Dan Roujinsky <danr@siemplify.co>
2020-06-07 09:43:05 +0300

    chore: add lazy route

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
+  route;
+
   constructor() { }
 
   ngOnInit() {
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NavigationModule } from '../navigation/navigation.module';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
@@ -9,7 +11,22 @@ import { UsersComponent } from './users.component';
   declarations: [UsersComponent],
   imports: [
     CommonModule,
-    UsersRoutingModule
+    UsersRoutingModule,
+    NavigationModule.forChild({
+      baseRoute: 'users',
+      dataKey: 'data',
+      routes: [
+        {
+          id: 'user',
+          route: 'users/user'
+        },
+        {
+          id: 'test',
+          route: 'ext/test'
+        }
+      ]
+    }),
+    FormsModule
   ]
 })
 export class UsersModule { }
